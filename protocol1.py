787bf00f9705f5ca2f61be76c535cbc2914da464
Nathan Montanez <71854770+NateTheGreat7117@users.noreply.github.com>
2023-07-25 19:07:31 -0400

    Update protocol1.py

--- protocol1.py
+++ protocol1.py
@@ -1,4 +1,6 @@
 import argparse
+import win32api
+import win32gui
 import pynput
 import time
 
@@ -11,7 +13,13 @@ class input_controller:
         self.current = set()
 
         self.type = type.lower()
-        self.activate = True
+
+        self.keyboard_listener = pynput.keyboard.Listener(on_press=self.on_press,
+                                                          on_release=self.on_release)
+        self.mouse_listener = pynput.mouse.Listener(suppress=True)
+        self.suppressed = False
+
+        self.keyboard_listener.start()
 
     def on_press(self, key):
         try:
@@ -19,10 +27,22 @@ class input_controller:
                 self.current.add(key)
 
                 if all(k in self.current for k in self.COMBO):
-                    print("Stopping")
-                    if "x" in self.type:
-                        self.mouse_listener.stop()
-                        self.activate = False
+                    if self.suppressed:
+                        self.suppressed = False
+                        print("Stopping")
+                        if "x" in self.type or "z" in self.type:
+                            self.mouse_listener.stop()
+                        if "y" in self.type or "z" in self.type:
+                            self.toggle_mic()
+                    else:
+                        print("Continuing")
+                        self.suppressed = True
+                        if "x" in self.type or "z" in self.type:
+                            self.mouse_listener = pynput.mouse.Listener(suppress=True)
+                            self.mouse_listener.start()
+                        if "y" in self.type or "z" in self.type:
+                            self.toggle_mic()
+                    time.sleep(1)
         except:
             pass
 
@@ -33,36 +53,34 @@ class input_controller:
         except:
             pass
 
+    def toggle_mic(self):
+        WM_APPCOMMAND = 0x319
+        APPCOMMAND_MICROPHONE_VOLUME_MUTE = 0x180000
+
+        hwnd_active = win32gui.GetForegroundWindow()
+        win32api.SendMessage(hwnd_active, WM_APPCOMMAND, None, APPCOMMAND_MICROPHONE_VOLUME_MUTE)
+
     def run(self):
-        self.keyboard_listener = pynput.keyboard.Listener(on_press=self.on_press,
-                                                          on_release=self.on_release)
-        self.mouse_listener = pynput.mouse.Listener(suppress=True)
-
-        self.keyboard_listener.start()
-
-        if "x" in self.type:
+        if "x" in self.type or "z" in self.type:
             self.mouse_listener.start()
             print("Disabling mouse")
+            self.suppressed = True
+        if "y" in self.type or "z" in self.type:
+            self.toggle_mic()
 
-        while self.activate:
-            # if self.keyboard_listener.running:
-            #     pass
-            # if self.mouse_listener.running:
-            #     pass
-            time.sleep(.1)
+        while self.run:
             pass
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''
-    Program to disable basic inputs to pc such as keyboard, mouse, webcam, and microphone
+    Program to disable basic inputs to pc such as keyboard, mouse, and microphone
     ''')
 
     parser.add_argument("--type", type=str, default="wx",
-                        help="the inputs to disable(w=keyboard,x=mouse,y=webcam,z=microphone")
+                        help="the inputs to disable(w=keyboard,x=mouse,y=microphone,z=all")
 
     args = parser.parse_args()
 
-    # controller = input_controller(args.type)
-    controller = input_controller("x")
+    controller = input_controller(args.type)
     controller.run()
